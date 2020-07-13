a3bd4ec7c8f8dffc96d628e6c4606b9f50c571e8
niknov <n.novozhilov80@gmail.com>
2020-07-13 17:13:14 +0300

    Поиск в гугле

--- content_script.js
+++ content_script.js
@@ -1,18 +1,28 @@
 let title = document.getElementById('title');
-
+let price = document.getElementById('price_inside_buybox');
 
 let textTitle = title.textContent;
+let textPrice = price.textContent;
+let str = textPrice.replace('$','');
+let resultPrice = str / 2;
+let searchPrice = textTitle + " $1..." + resultPrice;
+
+console.log("textPrice:" + resultPrice);
 let el = textTitle;
+// let txt = 'Hello world!';
 
 
 let h2 = document.createElement("h2");
 let btn = document.createElement("a");
 let btnInText = document.createTextNode('Найти в Google');
+let staticURL = 'https://www.google.com/search?q=';
+let reqURL = encodeURI(searchPrice);
 
-btn.href = 'https://www.google.com/search?q=';
+
+btn.href = staticURL + reqURL;
 btn.setAttribute('target', '_blank');
 
-title.append(el, h2);
+// title.append(el, h2);
 title.append(el, btn);
 
 
